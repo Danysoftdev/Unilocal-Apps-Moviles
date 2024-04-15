f9a659b1e10f9a275b15975698603d3f951c0ce1
Danysoftdev <danysoftdev@gmail.com>
2024-04-14 20:54:15 -0500

    Creado fragmento encabezado simple
    Implementado en el detalle del lugar y renombrados algunos archivos

--- app/src/main/java/co/edu/eam/unilocal/activities/DetailPlaceActivity.kt
+++ app/src/main/java/co/edu/eam/unilocal/activities/DetailPlaceActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import co.edu.eam.unilocal.R
 
-class DetalleLugarActivity : AppCompatActivity() {
+class DetailPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detalle_lugar)
+        setContentView(R.layout.activity_detail_place)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
