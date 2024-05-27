e56a16c003aa71a008e5dedd1f13fee0f7c4b464
Juan Manuel <arenasrinconj@gmail.com>
2024-05-27 15:33:03 -0500

    se edita la clase usuario

--- app/src/main/java/co/edu/eam/unilocal/models/Usuario.kt
+++ app/src/main/java/co/edu/eam/unilocal/models/Usuario.kt
@@ -1,8 +1,18 @@
 package co.edu.eam.unilocal.models
 
-class Usuario (var id:Int, var nombre: String, var nickname: String, var correo: String, var password: String, var tipo: String) {
+class Usuario () {
 
+    var id:Int =0
+    var nombre:String = ""
+    var nickname: String = ""
+    var correo: String = ""
+    var password: String = ""
+    var tipo:String = ""
     var favoritos:ArrayList<Lugar> = ArrayList()
+
+    constructor(var id:Int, var nombre: String, var nickname: String, var correo: String, var password: String, var tipo: String){
+
+    }
 
     override fun toString(): String {
         return "Usuario(id=$id, nombre='$nombre', nickname='$nickname', correo='$correo', password='$password', tipo='$tipo')"
